38a9d2a59cd3ff66534f9c48c50490417ac4d45c
johnsmith0976 <107722299+johnsmith0976@users.noreply.github.com>
2024-02-06 13:38:21 -0500

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,5 +1,29 @@
+import java.util.*;
+
 public class ATM {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, please input your information");
+        System.out.println("Name:");
+        String name = input.nextLine();
+        System.out.println("Starting credit card balance:");
+        double balance = input.nextDouble();
+        System.out.println("Starting savings balance:");
+        double sbalance = input.nextDouble();
+        System.out.println("Starting checking balance:");
+        double cbalance = input.nextDouble();
+        System.out.println("Credit APR:");
+        double apr = input.nextDouble();
+        System.out.println("Overdraft fee:");
+        double fee = input.nextDouble();
+        System.out.println("Interest rate:");
+        double interest = input.nextDouble();
+
+        while (true) {
+            
+            
+
+
+        }
     }
 }
